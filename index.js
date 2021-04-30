a3f802cd38816d8a52c10a0a45b17611d24f197b
Jayar Iglesias <jayariglesias@gmail.com>
2021-04-30 19:34:09 +0800

    ext added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 global.include = (param) => {
     let replaced = __dirname.replace('/node_modules/node-absolute-path', '');
     let directory = param;
-    let ext = ['', '.ts', '.tsx', '.js', '.jsx'];
+    let ext = ['', '.ts', '.tsx', '.js', '.jsx','.html','.css','.png','.jpg'];
 
     for (let i = 0; i < ext.length; i++) {
         if (fs.existsSync(`${replaced}/${param}${ext[i]}`)) {
