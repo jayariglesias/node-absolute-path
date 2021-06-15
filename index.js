0adcf4f2761fe9f250f1aba32f9c0ead69cd11d6
jeee <jayariglesias@gmail.com>
2021-06-15 17:58:18 +0800

    updated

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs')
 
 global.include = (param) => {
-    let x = __dirname;
-    x = __dirname.replace(/\\/g, "/")
+    let x =  __dirname.replace(/\\/g, "/");
     let y = x.replace('/node_modules/node-absolute-path', '');
     let z = param;
     let ext = ['', '.ts', '.tsx', '.js', '.jsx','.html','.css','.png','.jpg'];
