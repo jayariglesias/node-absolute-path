ff79b4ce0a967a8369710c8d82635c6990a154b7
JEEE <52460972+jayariglesias@users.noreply.github.com>
2021-06-15 17:46:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 
 global.include = (param) => {
-    let path = __dirname;
-    path = __dirname.replace(/\\/g, "/")
-    let replaced = path.replace('/node_modules/node-absolute-path', '');
-    let directory = param;
+    let x = __dirname;
+    x = __dirname.replace(/\\/g, "/")
+    let y = x.replace('/node_modules/node-absolute-path', '');
+    let z = param;
     let ext = ['', '.ts', '.tsx', '.js', '.jsx','.html','.css','.png','.jpg'];
 
     for (let i = 0; i < ext.length; i++) {
-        if (fs.existsSync(`${replaced}/${param}${ext[i]}`)) {
-            directory = `${replaced}/${param}`;
+        if (fs.existsSync(`${y}/${param}${ext[i]}`)) {
+            z = `${y}/${param}`;
         }
     };
 
