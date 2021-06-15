09342c46116e24b7a172f87e009768b381352573
jeee <jayariglesias@gmail.com>
2021-06-15 18:03:13 +0800

    optimized

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const fs = require('fs')
 
-global.include = (param) => {
-    let x =  __dirname.replace(/\\/g, "/");
+global.include = (z) => {
+    let x = __dirname.replace(/\\/g, "/");
     let y = x.replace('/node_modules/node-absolute-path', '');
-    let z = param;
-    let ext = ['', '.ts', '.tsx', '.js', '.jsx','.html','.css','.png','.jpg'];
+    let ext = ['', '.ts', '.tsx', '.js', '.jsx', '.html', '.css', '.png', '.jpg'];
 
     for (let i = 0; i < ext.length; i++) {
-        if (fs.existsSync(`${y}/${param}${ext[i]}`)) {
-            z = `${y}/${param}`;
+        if (fs.existsSync(`${y}/${z}${ext[i]}`)) {
+            z = `${y}/${z}`;
         }
     };
 
-    return require(directory);
+    return require(z);
 }
